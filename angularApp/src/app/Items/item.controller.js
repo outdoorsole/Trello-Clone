8fb5c66971ce183521b0b3a3bcccb1d7357c581b
Maribel Gordon <outdoorsole@users.noreply.github.com>
2015-11-12 15:39:11 -0800

    Update to item controller to use vm variable instead of . Fix console log statements.

--- angularApp/src/app/Items/item.controller.js
+++ angularApp/src/app/Items/item.controller.js
@@ -2,65 +2,65 @@
 'use strict';
 
 angular.module('mytodo')
-  .controller('ItemController', function ($scope, $http) {
-
+  .controller('ItemController', function ($http, $log) {
+    var vm = this;
     // All of this is happening on load (until methods below)
 
     // This variable stores the form data coming through the front-end
-    $scope.formData = {};
+    vm.formData = {};
 
     // This variable stores the items list from the database
-    $scope.items = [];
+    vm.items = [];
 
     // when landing on the page, get all todos and show them
 
     $http.get('/api/items')
       .success(function(data) {
-        $scope.title = "My List of Items";
-        console.log('I got the data I requested');
-        console.log('--------------------------');
-        console.log('This is $scope.items: ', $scope.items);
-        $scope.items = data;
-        console.log('--------------------------');
-        console.log('This is $scope.items: ', $scope.items);
+        vm.title = "My List of Items";
+        // console.log('I got the data I requested');
+        // console.log('--------------------------');
+        // console.log('This is vm.items: ', vm.items);
+        vm.items = data;
+        // console.log('--------------------------');
+        // console.log('This is vm.items: ', vm.items);
       })
 
-    $scope.createItem = function () {
-      console.log('This is inside of createItem: ');
-      console.log('This is formData: ', $scope.formData);
-      $http.post('/api/items/create', $scope.formData)
+    vm.createItem = function () {
+      // console.log('This is inside of createItem: ');
+      // console.log('This is formData: ', vm.formData);
+      $http.post('/api/items/create', vm.formData)
         .success(function(data) {
-          $scope.items = data;
-          console.log(data);
+          vm.items = data;
+          $log.log(data);
         })
         .error(function(data) {
-          console.log('Error: ' + data);
+          $log.log('Error: ' + data);
       });
     };
 
-    $scope.removeTodo = function (itemId) {
-      console.log('This is inside of removeTodo: ');
-      console.log('This is formData: ', $scope.formData);
+    vm.removeTodo = function (itemId) {
+      // console.log('This is inside of removeTodo: ');
+      // console.log('This is formData: ', vm.formData);
       $http.post('/api/items/delete/' + itemId)
         .success(function(data) {
-          $scope.items = data;
-          console.log(data);
+          vm.items = data;
+          $log.log(data);
         })
         .error(function(data) {
-          console.log('Error: ' + data);
+          $log.log('Error: ' + data);
         });
     };
 
-    $scope.updateTodo = function (itemId, item_name) {
-      console.log('This is inside of updateTodo: ');
-      console.log('This is the item_name: ', item_name);
+    vm.updateTodo = function (itemId, item_name) {
+      // console.log('This is inside of updateTodo: ');
+      // console.log('This is the item_name: ', item_name);
       $http.post('/api/items/update/' + itemId + '?item_name=' + item_name)
         .success(function(data) {
-          $scope.items = data;
-          console.log(data);
+          vm.items = data;
+          $log.log(data);
         })
         .error(function(data) {
-          console.log('Error: ' + data);
+          $log.log('Error: ' + data);
         });
     };
   })
