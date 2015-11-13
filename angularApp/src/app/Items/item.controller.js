e818634c89ab5d3e2975d56f105881f6992a7fcc
Maribel Gordon <outdoorsole@users.noreply.github.com>
2015-11-12 17:20:36 -0800

    Add item service -- not currently working.

--- angularApp/src/app/Items/item.controller.js
+++ angularApp/src/app/Items/item.controller.js
@@ -2,7 +2,7 @@
 'use strict';
 
 angular.module('mytodo')
-  .controller('ItemController', function ($http, $log) {
+  .controller('ItemController', ['ItemService', function (ItemService, $log) {
     var vm = this;
     // All of this is happening on load (until methods below)
 
@@ -14,7 +14,7 @@ angular.module('mytodo')
 
     // when landing on the page, get all todos and show them
 
-    $http.get('/api/items')
+    ItemService.get('/api/items')
       .success(function(data) {
         vm.title = "My List of Items";
         // console.log('I got the data I requested');
@@ -28,7 +28,7 @@ angular.module('mytodo')
     vm.createItem = function () {
       // console.log('This is inside of createItem: ');
       // console.log('This is formData: ', vm.formData);
-      $http.post('/api/items/create', vm.formData)
+      ItemService.post('/api/items/create', vm.formData)
         .success(function(data) {
           vm.items = data;
           $log.log(data);
@@ -41,7 +41,7 @@ angular.module('mytodo')
     vm.removeTodo = function (itemId) {
       // console.log('This is inside of removeTodo: ');
       // console.log('This is formData: ', vm.formData);
-      $http.post('/api/items/delete/' + itemId)
+      ItemService.post('/api/items/delete/' + itemId)
         .success(function(data) {
           vm.items = data;
           $log.log(data);
@@ -54,7 +54,7 @@ angular.module('mytodo')
     vm.updateTodo = function (itemId, item_name) {
       // console.log('This is inside of updateTodo: ');
       // console.log('This is the item_name: ', item_name);
-      $http.post('/api/items/update/' + itemId + '?item_name=' + item_name)
+      ItemService.post('/api/items/update/' + itemId + '?item_name=' + item_name)
         .success(function(data) {
           vm.items = data;
           $log.log(data);
@@ -63,5 +63,5 @@ angular.module('mytodo')
           $log.log('Error: ' + data);
         });
     };
-  })
+  }])
 })();
