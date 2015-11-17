0c72d54cc910bdddf16dedd2b853107c203b5272
Maribel Gordon <outdoorsole@users.noreply.github.com>
2015-11-16 16:17:09 -0800

    Add 2nd test for list controller (testing database to create a list) - passing.

--- expressApp/spec/controllers/list_spec.js
+++ expressApp/spec/controllers/list_spec.js
@@ -3,7 +3,7 @@ var app = require('../../app').app;
 
 var List = require('../../app/models/list');
 
-// Test 1 - check if an showLists returns an empty list when there is no data in the database
+// Test 1 - check if showLists returns an empty list when there is no data in the database
 describe('ListController', function () {
   describe('with no data', function () {
     it('should return an empty list', function (done) {
@@ -15,6 +15,49 @@ describe('ListController', function () {
           done.fail(err);
         } else {
           expect(res.body).toEqual([]);
+          done();
+        }
+      });
+    });
+  });
+
+// Test 2 - check if showList returns data when there is information in the database
+  describe('with data', function() {
+    var testList;
+
+    beforeEach(function(done) {
+      List.create({ list_name: 'test list' }, function(err, newList) {
+        if (err) {
+          done.fail(err);
+        } else {
+          testList = newList;
+          done();
+        }
+      });
+    });
+
+    it('should return a list', function(done) {
+      request(app).get('/api/lists')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res){
+        if (err) {
+          done.fail(err);
+        } else {
+          expect(res.body.length).toEqual(1);
+          returnedList = res.body[0];
+          expect(returnedList).toBeDefined();
+          expect(returnedList.list_name).toEqual(testList.list_name);
+          done();
+        }
+      });
+    });
+
+    afterEach(function(done) {
+      testList.remove(function(err, removedList) {
+        if (err) {
+          done.fail(err);
+        } else {
           done();
         }
       });
