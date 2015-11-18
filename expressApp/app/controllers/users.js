d0b4e728fbb5ec6e2eb34abf54ce8f49f7be85dc
Maribel Gordon <outdoorsole@users.noreply.github.com>
2015-11-18 09:42:51 -0800

    Add user 'createUser' action for the User controller in express app.

--- expressApp/app/controllers/users.js
+++ expressApp/app/controllers/users.js
@@ -15,3 +15,20 @@ exports.showUsers = function (req, res) {
     }
   });
 }
+
+exports.createUser = function (req, res) {
+  var user = new User({
+    user_name: req.body.user_name
+  })
+  user.save(function(err, savedUser) {
+    if (savedUser) {
+      User.find( { user_name: req.body.user_name}, function(error, returnedUser) {
+        if (returnedUser) {
+          res.json(returnedUser)
+        } else if (err) {
+          console.log('Failed to save: ' + err);
+        }
+      })
+    }
+  })
+}
