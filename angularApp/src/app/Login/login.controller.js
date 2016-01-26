19a01da686c67baee498b65d3a26837099b1ee05
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-01-26 12:05:19 -0800

    Uncomment the controllers for Login and Signup - Fix the variable for scope (this)

--- angularApp/src/app/Login/login.controller.js
+++ angularApp/src/app/Login/login.controller.js
@@ -1,17 +1,17 @@
-// (function() {
-//   'use strict';
+(function() {
+  'use strict';
 
-//   angular.module('mytodo')
-//   .controller('LoginController', function() {
-//     // All of this is happening on load (until methods below)
-//     var vm.this;
+  angular.module('mytodo')
+  .controller('LoginController', function() {
+    // All of this is happening on load (until methods below)
+    var vm = this;
 
-//     // This variable stores the form data coming through the front-end
-//     vm.formData = {};
+    // This variable stores the form data coming through the front-end
+    vm.formData = {};
 
-//     // Sign up a new user from the form data
-//     vm.loginUser = function() {
-//       console.log('This is the LoginController');
-//     }
-//   })
-// })()
+    // Sign up a new user from the form data
+    vm.loginUser = function() {
+      console.log('This is the LoginController');
+    }
+  })
+})()
