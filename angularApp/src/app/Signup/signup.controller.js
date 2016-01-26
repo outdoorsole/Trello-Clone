4d5f0736f215744b58230e8ad804898909e1623c
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-01-26 03:51:01 -0800

    write the skeleton of the SignupController for client app.

---
+++ angularApp/src/app/Signup/signup.controller.js
@@ -0,0 +1,17 @@
+function() {
+  'use strict';
+
+  angular.module('mytodo')
+  .controller('SignupController', function() {
+    // All of this is happening on load (until methods below)
+    var vm.this;
+
+    // This variable stores the form data coming through the front-end
+    vm.formData = {};
+
+    // Sign up a new user from the form data
+    vm.signupUser = function() {
+
+    }
+  })
+}
