e205badf93247d377b6b71e94c2ce8e89d97eaa3
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-01-26 00:51:03 -0800

    Add route for authentication for user.

--- expressApp/app.js
+++ expressApp/app.js
@@ -74,6 +74,40 @@ app.route('/login')
     res.send('processing the login form!');
   });
 
+// Route to authenticate a user (POST http://localhost:3000/api/authenticate)
+app.post('/authenticate', function(req, res) {
+
+  // find the User
+  User.findOne({ name: req.body.name }, function (req, res) {
+    if (error) {
+      throw error;
+    }
+    if (!user) {
+      res.json({ success: false, message: 'Authentication failed. User not found.' });
+    } else if (user) {
+
+      // check if password matches
+      if (user.password != req.body.password) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+
+        // if user is found and password is right create a token
+        var token = jwt.sign(user, app.get('superSecret'), {
+          expiresInMinutes: 1440 // expires in 24 hours
+        });
+
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }
+    }
+  });
+});
+
+
 //--------------------------------------------------------------
 // Route to show a random message (GET http://localhost:3000/api/)
 app.get('/', function(req, res) {
