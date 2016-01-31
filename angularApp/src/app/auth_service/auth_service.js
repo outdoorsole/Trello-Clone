f19bd0689063672bff5a725f02c90eeb4a8e6f4b
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-01-31 14:26:39 -0800

    Update auth_service file with sample code template

--- angularApp/src/app/auth_service/auth_service.js
+++ angularApp/src/app/auth_service/auth_service.js
@@ -1,55 +1,50 @@
-// (function() {
-//   'use strict';
+(function () {
+  'use strict';
 
-//   angular
-//   .module('mytodo')
-//   .factory('AuthenticationService', AuthenticationService);
+  angular
+      .module('mytodo')
+      .factory('AuthenticationService', AuthenticationService);
 
-//   AuthenticationService.$inject = ['$log', '$http', '$cookieStore', '$rootScope', '$timeout'];
-//   function AuthenticationService($log, $http, $cookieStore, $rootscope, $timeout) {
-//     var service = {};
+  AuthenticationService.$inject = ['$http', '$cookieStore', '$rootScope', '$timeout'];
+  function AuthenticationService($http, $cookieStore, $rootScope, $timeout) {
+    var service = {};
 
-//     service.login = login;
-//     service.SetCredentials = SetCredentials;
-//     service.ClearCredentials = ClearCredentials;
+    service.login = login;
+    service.setCredentials = setCredentials;
+    service.clearCredentials = clearCredentials;
 
-//   // Show current user logged in:
-//   return service;
+    // Show current user logged in:
+    return service;
 
-//   // Use form information to signup. The callback will receive an object with the username and token
-//   function signUp(email, password, callback) {
+    // Use form information (email & password) to login. The callback will receive an object with the username and token
+    function login(email, password, callback) {
 
-//     $http.post('/authenticate', {email: email, password: password})
-//     .success(function(res) {
-//       callback({
-//         success: res.success,
-//         email: email,
-//         token: res.token,
-//         id: res._id
-//       });
-//     });
-//   }
-//   // Store credentials for reuse. They are stored in $rootScope for the current app session. Stored in the $cookieStore for use if the app is reloaded
-//   function SetCredentials(email, token, id) {
-//     $rootScope.globals = {
-//       currentUser: {
-//         email: data.email,
-//         token: data.name,
-//         id: data.id,
-//         token: data.token
-//       }
-//     }
-//   }
+      $http.post('/authenticate', { email: email, password: password })
+         .success(function (res) {
+             callback({ email: email, token: res.body.token });
+         });
 
-//   $http.defaults.headers.common['X-ACCESS-TOKEN'] = data.token;
-//   $cookieStore.put('globals', $rootScope.globals);
-// }
+    }
 
-//   // Cleanup the stored credentials
-//   function clearCredentials() {
-//     $log.log('clear creds');
-//     $rootScope.globals = {};
-//     $cookieStore.remove('globals');
-//     $http.defaults.headers.common.Authorization = 'Basic';
-//   }
-// })();
+    // Store credentials for reuse. They are stored in $rootScope for the current app session. Stored in the $cookieStore for use if the app is reloaded
+    function setCredentials(email, token) {
+      $rootScope.globals = {
+        currentUser: {
+          email: email,
+          token: token
+        }
+      };
+
+      $http.defaults.headers.common['X-ACCESS-TOKEN'] = token;
+      $cookieStore.put('globals', $rootScope.globals);
+    }
+
+    // Cleanup the stored credentials
+    function clearCredentials() {
+      $log.log('clear creds');
+      $rootScope.globals = {};
+      $cookieStore.remove('globals');
+      $http.defaults.headers.common.Authorization = 'Basic';
+    }
+  }
+})();
