e0a497971601054677e6acc9137cc22e2e9b611d
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-02-01 15:47:45 -0800

    Able to pass the correct login info to the auth service.

--- angularApp/src/app/auth_service/auth_service.js
+++ angularApp/src/app/auth_service/auth_service.js
@@ -18,12 +18,21 @@
 
     // Use form information (email & password) to login. The callback will receive an object with the username and token
     function login(email, password, callback) {
+      console.log('Inside the login function in auth: ');
+      console.log('This is the email: ', email);
+      console.log('This is the password: ', password);
+      console.log('This si the callback: ', callback);
 
-      $http.post('/api/login', { email: email, password: password })
-         .success(function (res) {
-             callback({ email: email, token: res.body.token });
-         });
-
+      $http.post('/api/login', {
+        email: email,
+        password: password
+      })
+      .success(function (res) {
+        callback({
+          email: email,
+          token: res.body.token
+        });
+      });
     }
 
     // Store credentials for reuse. They are stored in $rootScope for the current app session. Stored in the $cookieStore for use if the app is reloaded
