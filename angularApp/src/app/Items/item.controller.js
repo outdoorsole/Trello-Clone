bd006d7116b9d40b21b2d2d31e649f9836dfbb36
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-02-06 15:21:42 -0800

    Rewrite BoardService function (for hoisting) in item.controller.js file.

--- angularApp/src/app/Items/item.controller.js
+++ angularApp/src/app/Items/item.controller.js
@@ -1,7 +1,13 @@
 (function() {
 'use strict';
 
-  var ItemController = function (ItemService, $log) {
+  angular.module('mytodo')
+  .controller('ItemController', ItemController);
+
+  // $inject Property Annotation: an array of service names to inject to the controller.
+  ItemController.$inject = ['ItemService', '$log'];
+
+  function ItemController(ItemService, $log) {
     // All of this is happening on load (until methods below)
     var vm = this;
 
@@ -26,46 +32,40 @@
     // create a new item
     vm.createItem = function(listId, formData) {
       ItemService.createItem(listId, formData)
-        .then(function(item) {
-          vm.items.push(item);
-        })
-        .catch(function(err) {
-          $log.error('Error fetching items: ', err);
-        });
+      .then(function(item) {
+        vm.items.push(item);
+      })
+      .catch(function(err) {
+        $log.error('Error fetching items: ', err);
+      });
     }
 
     vm.removeItem = function (itemId) {
       ItemService.removeItem(itemId)
-        .then(function(deletedItem) {
-          for (var i = 0; i < vm.items.length; i++) {
-            if (vm.items[i]._id === deletedItem._id) {
-              vm.items.splice(i, 1);
-            }
+      .then(function(deletedItem) {
+        for (var i = 0; i < vm.items.length; i++) {
+          if (vm.items[i]._id === deletedItem._id) {
+            vm.items.splice(i, 1);
           }
-        })
-        .catch(function(err) {
-          $log.error('Error fetching items: ', err);
-        });
+        }
+      })
+      .catch(function(err) {
+        $log.error('Error fetching items: ', err);
+      });
     }
 
     vm.updateItem = function (itemId, itemName) {
       ItemService.updateItem(itemId, itemName)
-        .then(function(data) {
-          for (var i = 0; i < vm.items.length; i++) {
-            if (vm.items[i].id === itemId) {
-              vm.items[i] = data;
-            }
+      .then(function(data) {
+        for (var i = 0; i < vm.items.length; i++) {
+          if (vm.items[i].id === itemId) {
+            vm.items[i] = data;
           }
-        })
-        .catch(function(data) {
-          $log.log('Error: ' + data);
-        });
+        }
+      })
+      .catch(function(data) {
+        $log.log('Error: ' + data);
+      });
     };
   }
-
-  // $inject Property Annotation: an array of service names to inject to the controller.
-  ItemController.$inject = ['ItemService', '$log'];
-
-  angular.module('mytodo')
-  .controller('ItemController', ItemController);
 })();
