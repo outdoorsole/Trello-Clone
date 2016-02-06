00c41ebcbaffcc5e0767854a8e5a4d5fb093af6e
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-02-06 15:26:32 -0800

    Refactor user_service.js file to use syntax for dependency injection. - Use '$inject property annotation' to pass the services to the controller - Rewrite UserService function (for hoisting).

--- angularApp/src/app/user_service/user_service.js
+++ angularApp/src/app/user_service/user_service.js
@@ -2,64 +2,69 @@
   'use strict';
 
   angular.module('mytodo')
-    .factory('UserService', ['$http', '$q', '$log', function($http, $q, $log) {
-      var service = {
-        getUsers: getUsers,
-        createUser: createUser,
-        removeUser: removeUser,
-        updateUser: updateUser
-      }
+    .factory('UserService', UserService);
 
-      function getUsers() {
-        var deferred = $q.defer();
-        $http.get('api/users')
-        .success(function (returnedUsers) {
-          deferred.resolve(returnedUsers);
+  // $inject Property Annotation: an array of service names to inject to the controller.
+  UserService.$inject = ['$http', '$q', '$log'];
+
+  function UserService($http, $q, $log) {
+    var service = {
+      getUsers: getUsers,
+      createUser: createUser,
+      removeUser: removeUser,
+      updateUser: updateUser
+    }
+
+    function getUsers() {
+      var deferred = $q.defer();
+      $http.get('api/users')
+      .success(function (returnedUsers) {
+        deferred.resolve(returnedUsers);
+      })
+      .error(function (data) {
+        deferred.reject('Error: ', data);
+        $log.log('Error: ', data);
+      });
+      return deferred.promise;
+    }
+
+    function createUser(formData) {
+      var deferred = $q.defer();
+      $http.post('/api/user/create', formData)
+        .success(function(createdUser) {
+          deferred.resolve(createdUser);
         })
-        .error(function (data) {
+        .error(function(data) {
           deferred.reject('Error: ', data);
-          $log.log('Error: ', data);
-        });
-        return deferred.promise;
-      }
-
-      function createUser(formData) {
-        var deferred = $q.defer();
-        $http.post('/api/user/create', formData)
-          .success(function(createdUser) {
-            deferred.resolve(createdUser);
-          })
-          .error(function(data) {
-            deferred.reject('Error: ', data);
-            $log.log('Error: ' + data);
-          });
-          return deferred.promise;
-      }
-
-      function removeUser (userId) {
-        var deferred = $q.defer();
-        $http.post('/api/user/delete/' + userId)
-        .success(function(data) {
-          deferred.resolve(data);
-        })
-        .error(function(data) {
           $log.log('Error: ' + data);
         });
         return deferred.promise;
-      }
+    }
 
-      function updateUser(userId, userName) {
-        var deferred = $q.defer();
-        $http.post('/api/user/update/' + userId + '?user_name=' + userName)
-        .success(function(data) {
-          deferred.resolve(data);
-        })
-        .error(function(data) {
-          $log.log('Error: ' + data);
-        });
-        return deferred.promise;
-      }
+    function removeUser (userId) {
+      var deferred = $q.defer();
+      $http.post('/api/user/delete/' + userId)
+      .success(function(data) {
+        deferred.resolve(data);
+      })
+      .error(function(data) {
+        $log.log('Error: ' + data);
+      });
+      return deferred.promise;
+    }
 
-      return service;
-    }]);
+    function updateUser(userId, userName) {
+      var deferred = $q.defer();
+      $http.post('/api/user/update/' + userId + '?user_name=' + userName)
+      .success(function(data) {
+        deferred.resolve(data);
+      })
+      .error(function(data) {
+        $log.log('Error: ' + data);
+      });
+      return deferred.promise;
+    }
+
+    return service;
+  }
 })();
